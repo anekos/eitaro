18233b28830b3ec3cbcb4930a2a6732cf2bce4bf
anekos <anekos@snca.net>
2019-10-30 21:09:24 +0900

    Change `export` command spces

--- src/command/export.rs
+++ src/command/export.rs
@@ -1,1 +1,35 @@
+use std::io::{BufRead, BufReader, BufWriter, stdout, Write, stdin};
+use std::path::Path;
+
 pub mod csv;
+
+use crate::dictionary::Dictionary;
+use crate::errors::AppResultU;
+
+
+
+trait Exporter {
+    fn export<T: Write>(&self, dictionary: &mut Dictionary, words: &[&str], out: &mut T) -> AppResultU;
+}
+
+
+pub fn export<T: AsRef<Path>>(dictionary_path: &T) -> AppResultU {
+    let mut dictionary = Dictionary::new(dictionary_path);
+    let exporter = csv::CsvExporter();
+
+    let input = stdin();
+    let input = input.lock();
+    let reader = BufReader::new(input);
+    let words = reader.lines().collect::<Result<Vec<String>, _>>()?;
+    let words: Vec<&str> = words.iter().map(String::as_ref).map(str::trim).collect();
+
+    let out = stdout();
+    let out = out.lock();
+    let mut out = BufWriter::new(out);
+
+    exporter.export(&mut dictionary, &words, &mut out)?;
+
+    out.flush()?;
+
+    Ok(())
+}
--- src/command/export/csv.rs
+++ src/command/export/csv.rs
@@ -1,6 +1,4 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, BufWriter, stdout, Stdout};
-use std::path::Path;
+use std::io::Write;
 
 use crate::errors::AppResultU;
 use crate::dictionary::Dictionary;
@@ -10,62 +8,41 @@ use super::Exporter;
 
 const MAX_LENGTH: usize = 500;
 
+pub struct CsvExporter();
 
-pub fn export<T: AsRef<Path>>(dictionary_path: &T) -> AppResultU {
-    let mut dic = Dictionary::new(dictionary_path);
+impl Exporter for CsvExporter {
+    fn export<T: Write>(&self, dictionary: &mut Dictionary, words: &[&str], out: &mut T) -> AppResultU {
+        let mut out = csv::Writer::from_writer(out);
 
-    let out = stdout();
-    let out = BufWriter::new(out);
-    let mut out = csv::Writer::from_writer(out);
+        for word in words {
+            if let Some(entries) = dictionary.get(&word)? {
+                let mut buf = "".to_owned();
 
-    for level in 0..255 {
-        read_level(&mut dic, dictionary_path, level, &mut out)?;
-    }
+                'outer: for entry in entries {
+                    for def in entry.definitions {
+                        for t in def.content {
+                            use crate::dictionary::Text::*;
 
-    out.flush()?;
-
-    Ok(())
-}
-
-
-fn read_level<T: AsRef<Path>>(dic: &mut Dictionary, dictionary_path: &T, level: u8, out: &mut csv::Writer<BufWriter<Stdout>>) -> AppResultU {
-    let mut path = dictionary_path.as_ref().to_path_buf();
-    path.push(format!("level-{}", level));
-
-    if !path.is_file() {
-        return Ok(());
-    }
-
-    let file = File::open(&path)?;
-    let file = BufReader::new(file);
-    let svl = format!("SVL{}", level);
-
-    for word in file.lines() {
-        let word = word?;
-        if let Some(entries) = dic.get(&word)? {
-            let mut buf = "".to_owned();
-
-            'outer: for entry in entries {
-                for def in entry.definitions {
-                    for t in def.content {
-                        use crate::dictionary::Text::*;
-
-                        if let Definition(s) = &t {
-                            if !buf.is_empty() {
-                                buf.push(' ');
-                            }
-                            buf.push_str(s);
-                            if MAX_LENGTH < buf.len() {
-                                break 'outer;
+                            if let Definition(s) = &t {
+                                if !buf.is_empty() {
+                                    buf.push(' ');
+                                }
+                                buf.push_str(s);
+                                if MAX_LENGTH < buf.len() {
+                                    break 'outer;
+                                }
                             }
                         }
                     }
                 }
+
+                out.write_record(&[word, buf.as_str()])?;
+            } else {
+                eprintln!("Definition not found: {}", word);
             }
-
-            out.write_record(&[&word, &buf, &svl])?;
         }
-    }
 
-    Ok(())
+        out.flush()?;
+        Ok(())
+    }
 }
