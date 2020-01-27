aa58ae34eee52ca827c95b875ee207e3d42652ff
anekos <anekos@snca.net>
2020-01-27 22:46:33 +0900

    Improve `analyze` command

--- src/command/analyze.rs
+++ src/command/analyze.rs
@@ -1,5 +1,6 @@
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
+use std::fmt;
 use std::io::{stdin, Read};
 use std::path::Path;
 
@@ -11,12 +12,15 @@ use crate::errors::AppResultU;
 
 
 
-#[derive(Eq, Hash, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 enum Level {
     Leveled(u8),
     OutOf,
     NotInDictionary,
 }
+
+struct LevelIter(Level);
+
 
 const CHARS: &str = r"[a-zA-Z]+";
 
@@ -26,9 +30,12 @@ pub fn analyze<T: AsRef<Path>>(dictionary_path: &T) -> AppResultU {
         v as f64 / total as f64 * 100.0
     }
 
-    let mut counts = HashMap::<Level, usize>::new();
-    let mut words = HashSet::<&str>::new();
-    let mut total = 0;
+    let mut words = HashMap::<&str, usize>::new();
+
+    let mut unique_counts = HashMap::<Level, usize>::new();
+    let mut unique_total = 0;
+    let mut cumulative_counts = HashMap::<Level, usize>::new();
+    let mut cumulative_total = 0;
 
     let mut dic = Dictionary::new(dictionary_path);
     let mut text = "".to_owned();
@@ -39,47 +46,104 @@ pub fn analyze<T: AsRef<Path>>(dictionary_path: &T) -> AppResultU {
     for word in chars.find_iter(&text) {
         let word = word.as_str();
         if 2 <= word.len() {
-            words.insert(word);
+            let count = words.entry(word).or_default();
+            *count += 1;
         }
     }
 
-    for word in words {
+    for (word, word_count) in words {
         let word = word.to_lowercase();
-        let count = if let Some(level) = dic.get_level(&word)? {
-            counts.entry(Level::Leveled(level)).or_default()
+        let level = if let Some(level) = dic.get_level(&word)? {
+            Level::Leveled(level)
         } else if dic.get(&word)?.is_some() {
-            counts.entry(Level::OutOf).or_default()
+            Level::OutOf
         } else {
-            counts.entry(Level::NotInDictionary).or_default()
+            Level::NotInDictionary
         };
-        *count += 1;
-        total += 1;
+
+        let unique_count = unique_counts.entry(level).or_default();
+        *unique_count += 1;
+        unique_total += 1;
+
+        let cumulative_count = cumulative_counts.entry(level).or_default();
+        *cumulative_count += word_count;
+        cumulative_total += word_count;
     }
 
-    println!("Level               Count       %    Accu");
+    println!(
+        "{:15} {:7}  {:>6}  {:>6}   {:7}  {:>6}  {:>6}",
+        "Level",
+        "Unique",
+        "%",
+        "Σ",
+        "Cumulu",
+        "%",
+        "Σ");
 
-    let mut acc = 0;
-    for level in 1 ..= 12 {
-        let count = counts.entry(Level::Leveled(level)).or_default();
-        acc += *count;
+    let mut unique_acc = 0;
+    let mut cumulative_acc = 0;
+
+    for level in LevelIter::new() {
+        let unique_count = unique_counts.entry(level).or_default();
+        unique_acc += *unique_count;
+        let cumulative_count = cumulative_counts.entry(level).or_default();
+        cumulative_acc += *cumulative_count;
         println!(
-            "Level {:>2}           {:>6}  {:>5.1}%  {:>5.1}%",
+            "{:15} {:>7}  {:>5.1}%  {:>5.1}%   {:>7}  {:>5.1}%  {:>5.1}%",
             level,
-            count.separated_string(),
-            pct(*count, total),
-            pct(acc, total));
+            unique_count.separated_string(),
+            pct(*unique_count, unique_total),
+            pct(unique_acc, unique_total),
+            cumulative_count.separated_string(),
+            pct(*cumulative_count, cumulative_total),
+            pct(cumulative_acc, cumulative_total));
     }
 
-    let ool = counts.entry(Level::OutOf).or_default().to_owned();
-    let nid = counts.entry(Level::NotInDictionary).or_default().to_owned();
-
-    acc += ool;
-    println!("Out of level       {:>6}  {:>5.1}%  {:>5.1}%", ool.separated_string(), pct(ool, total), pct(acc, total));
-    acc += nid;
-    println!("Not in dictionary  {:>6}  {:>5.1}%  {:>5.1}%", nid.separated_string(), pct(nid, total), pct(acc, total));
-
-    println!();
-
-    println!("Total:             {:>6}", total.separated_string());
+    println!(
+        "{:15} {:>7}                   {:>7}", "Total",
+        unique_total.separated_string(),
+        cumulative_total.separated_string());
     Ok(())
+}
+
+
+impl fmt::Display for Level {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Level::*;
+
+        match self {
+            Leveled(level) => f.pad(&format!("Level {:>2}", level)),
+            OutOf => f.pad("Out of level"),
+            NotInDictionary => f.pad("Not in dict"),
+        }
+    }
+}
+
+impl LevelIter {
+    fn new() -> Self {
+        LevelIter(Level::Leveled(0))
+    }
+}
+
+impl Iterator for LevelIter {
+    type Item = Level;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        use Level::*;
+
+        let result = match self.0 {
+            Leveled(level) if level < 12 =>
+                Leveled(level + 1),
+            Leveled(_) =>
+                OutOf,
+            OutOf =>
+                NotInDictionary,
+            NotInDictionary =>
+                return None,
+        };
+
+        self.0 = result;
+
+        Some(result)
+    }
 }
